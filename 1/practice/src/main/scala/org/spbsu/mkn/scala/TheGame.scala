e038c139bb24ca909e2ee832fa870938c64d261d
Marina.Pereskokova <vfhbyf2@yandex.ru>
2021-02-14 22:02:05 +0500

    first hw

--- 1/practice/src/main/scala/org/spbsu/mkn/scala/TheGame.scala
+++ 1/practice/src/main/scala/org/spbsu/mkn/scala/TheGame.scala
@@ -2,6 +2,7 @@ package org.spbsu.mkn.scala
 
 import scala.io.StdIn.readLine
 import scala.util.Random
+import scala.collection.mutable
 
 object TheGame {
 
@@ -12,13 +13,68 @@ object TheGame {
   class RepeatingDigitsException extends RuntimeException
   class WrongNumberLengthException(expected: Int, got: Int) extends RuntimeException
 
-  def generateNumberString(length: Int): String = ???
+  def generateNumberString(length: Int): String = {
+    val cur : mutable.HashSet[Int] = mutable.HashSet.empty
 
-  def validate(secret: String, userInput: String, numTries: Int = 1): GuessResult = ???
+    for (i <- 1 to length) {
+      var t = Random.between(0, 10)
+      while(cur.contains(t)) {
+        t = Random.between(0, 10)
+      }
+      cur.add(t)
+    }
+    val res : mutable.StringBuilder = new mutable.StringBuilder()
+    for (i <- Random.shuffle(cur.toSet.toList)) {
+      res.append(i.toString())
+    }
+
+    return res.toString()
+  }
+
+  def validate(secret: String, userInput: String, numTries: Int = 1): GuessResult = {
+    if (secret.length != userInput.length)
+      throw new WrongNumberLengthException(secret.length, userInput.length)
+
+    if (secret.toSet.size != secret.length)
+      throw new RepeatingDigitsException()
+
+    if (secret == userInput)
+      return Correct(numTries)
+
+    var a = 0
+    var b = 0
+    for (i <- 0 to secret.length - 1; if secret.charAt(i) == userInput.charAt(i))
+      a = a + 1
+
+    for (i <- '0' to '9'; if secret.toSet.contains(i) && userInput.contains(i))
+      b = b + 1
+
+    Incorrect(a, b - a)
+  }
 
   def main(args: Array[String]): Unit = {
     print("Enter your name: ")
     val name = readLine()
     println(s"Hello, $name!")
+
+    print("Enter the word length: ")
+    val len = readLine().toInt
+    val secret = generateNumberString(len)
+    var numTries = 1
+
+    var flag = false
+    while(!flag) {
+      println("Your turn!")
+      var s = readLine()
+      val res = validate(secret, s, numTries)
+      if (res.isInstanceOf[TheGame.Correct]) {
+        println("Win!!!")
+        println(res)
+        flag = true
+      }
+      else if (res.isInstanceOf[TheGame.Incorrect])
+        println(res)
+    }
+
   }
 }
